35b84bf65ad76d8acc9681120fd92012a83a8909
Toprak Mete Tezcan <toprakmetet2@gmail.com>
2025-08-30 01:20:47 +0300

    comment eklendi/added comments

--- Mini Web Proje/app.py
+++ Mini Web Proje/app.py
@@ -12,8 +12,9 @@ def submit():
     is_checked = "isChecked" in request.form
     with open("data.txt", "a") as f:
         f.write(f"Name {name}, Checked? {is_checked}\n")
-    # return f"Name: {name}, Checkbox checked? {is_checked}"
+       # return f"Name: {name}, Checkbox checked? {is_checked}"
+    # eğer inputu ekrana yazdırcaksan line 17'yi comment'le 15'i uncomment'le
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
